7ba288439055be100e4a400c4436b14cb44b73a4
nzweb <nzweb@vk.com>
2023-07-18 17:47:55 +0300

    feature: add router

---
+++ src/infrastruct/pages/CatalogPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>catalog</div>
+</template>
+
+<style scoped></style>
---
+++ src/infrastruct/router/routes.ts
@@ -0,0 +1,6 @@
+import { RouteRecordRaw } from 'vue-router';
+
+export const routes: RouteRecordRaw[] = [
+  { path: '/', component: () => import('../pages/HomePage.vue') },
+  { path: '/catalog', component: () => import('../pages/CatalogPage.vue') },
+];
